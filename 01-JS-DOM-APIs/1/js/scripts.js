70f575f53b5346b48f9551cd348555e529704ea3
pilar <pilarfernandezmutti@gmail.com>
2020-03-09 19:45:21 -0300

    topic 1 exercise 2

--- 01-JS-DOM-APIs/1/js/scripts.js
+++ 01-JS-DOM-APIs/1/js/scripts.js
@@ -5,3 +5,9 @@ window.onload = function(){
 document.getElementById('fadein').style.opacity = 1;
 
 } 
+
+function clickAlert(){
+    alert("Esto es un mensaje de alerta");
+}
+
+document.getElementById('boton').onclick= clickAlert;
